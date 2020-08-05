32ca4e12451c593be89bf31c4ac96f6b2e1df3da
Michael Valdron <michael.valdron@gmail.com>
2020-08-05 03:26:38 -0400

    core and core test assigned.

--- tests/test_package/test_core.py
+++ tests/test_package/test_core.py
@@ -1,9 +1,10 @@
+import package.core as core
 import unittest
 
 class TestCore(unittest.TestCase):
     """ Test cases for core module in package """
     def test_true(self):
-        assert True
+        assert core
 
 if __name__ == "__main__":
     unittest.main()
